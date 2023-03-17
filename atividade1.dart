aceb120af98a8f376624c6b88fd5b32666304a76
jhennycarvalho <jhennycarvalho>
2023-03-17 15:49:10 -0400

    commit init

---
+++ atividade1.dart
@@ -0,0 +1,9 @@
+import 'dart:math';
+
+void main() {
+  List numeros = List.generate(10, (index) => Random().nextInt(100));
+  print("Lista com 10 números inteiros aleatórios: $numeros");
+
+  numeros.removeAt(5); 
+  print("Lista após remover o valor da posição 5: $numeros");
+}
